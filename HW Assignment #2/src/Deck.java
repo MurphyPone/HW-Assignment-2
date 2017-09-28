39d110e2a70a90e61a2266d227aaf7ef18e319f3
MurphyPone <murphyP1@bishopireton.org>
2017-09-28 19:25:30 -0400

    :p

--- HW Assignment #2/src/Deck.java
+++ HW Assignment #2/src/Deck.java
@@ -8,17 +8,10 @@ public class Deck {
 	public Deck() {
 		this.myDeck = new Card[52]; //Creates an array with room for 52 cards
 		
-		//Creates cards
-		/*for(int s = 1; s < 5; s ++)
-			for(int r = 2; r < 14; r++ ) {	//TODO this loop is bad...
-				this.myDeck[s*r] = new Card(s-1, r);	//[s*r] is whack
-			}
-		*/ //Neat idea. but doesn't work
-		
 		int s = 0;	//Suit counter
 		while (s < 4 ) {		//Do 4x 
-			for(int r = 2; r < 14; r++) {	//r = rank value
-				this.myDeck[(14*s)+r] = new Card(s, r);	//TODO easy way to convert r ?????
+			for(int r = 2; r < 14; r++) {	//r = rank value	//Should run 13x4 
+				this.myDeck[(14*s)+r-2] = new Card(s, r);	//TODO easy way to convert r ?????
 			}
 			s++;	//Go to next suit
 		}
@@ -29,10 +22,9 @@ public class Deck {
 	public Deck(boolean notShuffled) {
 		this.myDeck = new Card[52]; //Creates an array with 52 slots
 		
-		if(notShuffled) 
-			System.out.print("AFASFASF");
+		if(notShuffled) {
 			//Deck();  //Can I just call the default constructor here?
-		else {	// need to give each card a random order
+		}else {	// need to give each card a random order
 			for(int i = 0; i < 52; i++ ) {	//Need 52 cards
 				Card tryC = new Card();	//Creates a random card
 				for(int c = 0; c < myDeck.length; c ++) {
